b22d3e341a6dda1a535eecdb68a910fede1f13f8
Andreas Scheipers <andreas.scheipers@stud.h-da.de>
2021-02-28 03:10:05 +0200

    Implement login function

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -4,7 +4,52 @@ import CustomLinkButton from './CustomLinkButton';
 import CustomButton from './CutomButton';
 import InputField from './InputField';
 
-class LoginScreen extends Component {  
+class LoginScreen extends Component {
+
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      emailOrAlias: '',
+      password: '',
+    };
+
+    this.login=this.login.bind(this);
+  }
+
+  async login() {
+    let reqData = {};
+
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(this.state.emailOrAlias)) {
+      reqData = {
+        email: this.state.emailOrAlias,
+        password: this.state.password,
+      }
+    } else {
+      reqData = {
+        alias: this.state.emailOrAlias,
+        password: this.state.password,
+      }
+    }
+
+    const res = await fetch('http://api.scheipe.rs/resell/v1/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(reqData)
+    });
+
+    const json = await res.json();
+    console.log(json)
+
+    if(res.status !== 200) {
+      alert('Login failed!')
+    } else {
+      alert('Login successfull!')
+    }
+  }
   
   render() {
     return (
